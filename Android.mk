3b6006baaff909e637aff1139b27da848245b4e5
Michael Bestas <mikeioannina@gmail.com>
2013-12-05 04:17:57 +0200

    mm-core: Use CM standard QCOM naming for 7x30
    Change-Id: I5a253f50f9800ba3419b0908f6a5895ff6615b80

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-media))
 
-ifneq ($(filter msm8960 msm7630_surf,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm7x30,$(TARGET_BOARD_PLATFORM)),)
 
 include $(call all-subdir-makefiles)
 
